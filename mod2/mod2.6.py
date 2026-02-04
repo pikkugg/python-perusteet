5f5d264bb2172fb85677c5c186803e57b0351eae
annag <anna.grohn@metropolia.fi>
2026-02-04 21:30:02 +0200

    teht. 6

---
+++ mod2/mod2.6.py
@@ -0,0 +1,15 @@
+import random
+
+#numerolukon koodi1:
+luku1 = random.randint(1,9)
+luku2 = random.randint(1,9)
+luku3 = random.randint(1,9)
+
+#numerolukon koodi2:
+luku4 = random.randint(1,6)
+luku5 = random.randint(1,6)
+luku6 = random.randint(1,6)
+luku7 = random.randint(1,6)
+
+print("eka numerolukon koodi voisi olla: ", luku1, luku2, luku3)
+print("toinen numerolukon koodi voisi olla: ", luku4, luku5, luku6, luku7)
