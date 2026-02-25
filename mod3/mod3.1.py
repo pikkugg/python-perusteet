8aac8e176bafaf5bb24f4e750a98b90083e897a9
annag <anna.grohn@metropolia.fi>
2026-02-25 13:11:43 +0200

    moduuli 3 tehtävät

--- mod3/mod3.1.py
+++ mod3/mod3.1.py
@@ -1,4 +1,7 @@
 pituus = float(input("kuinka pitkä kuha on?"))
 if  pituus <  37:
     print("kuha on alamittainen")
-    print(f"{pituus - 37}cm alle sallitun pituuden")
+    print(f"{37 - pituus}cm alle sallitun pituuden")
+
+else:
+    print("kuha on sopivan mittainen, voit pitää sen!")
